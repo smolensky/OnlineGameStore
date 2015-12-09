5bc52394768e598b6d0bc432507a368d2095d804
Bohdan <bohdan_smolenskyi@epam.com>
2015-12-09 19:33:15 +0200

    added hardcoded data to confid file

--- OnlineGameStore/OnlineGameStoreData/Migrations/Configuration.cs
+++ OnlineGameStore/OnlineGameStoreData/Migrations/Configuration.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using OnlineGameStoreData.Entities;
 
 namespace OnlineGameStoreData.Migrations
@@ -16,6 +17,7 @@ namespace OnlineGameStoreData.Migrations
 
         protected override void Seed(OnlineGameStoreData.DatabaseContext context)
         {
+            //Filling Platform types
             context.PlatformTypes.AddOrUpdate(
                 new PlatformTypeEntity { Type = "Mobile"},
                 new PlatformTypeEntity { Type = "Desktop"},
@@ -24,6 +26,7 @@ namespace OnlineGameStoreData.Migrations
                 );
             context.SaveChanges();
 
+            //Filling Genres
             context.Genres.AddOrUpdate(
                 new GenreEntity { Genre = "Strategy" },
                 new GenreEntity { Genre = "RPG" },
@@ -43,6 +46,45 @@ namespace OnlineGameStoreData.Migrations
                 new GenreEntity { Genre = "TPS", ParentGenre = "Action" },
                 new GenreEntity { Genre = "Misc.", ParentGenre = "Action" }
                 );
+            context.SaveChanges();
+
+            //Filling Games
+            context.Games.AddOrUpdate(
+                new GameEntity { Key = "1", Name = "GRID", Description = "Game with cool cars" },
+                new GameEntity { Key = "2", Name = "Diablo", Description = "Oldschool RPG" },
+                new GameEntity { Key = "3", Name = "Chess", Description = "Just chess" },
+                new GameEntity { Key = "4", Name = "The Cave", Description = "Indie game" }
+                );
+            context.SaveChanges();
+
+            //Filling Comments
+            context.Comments.AddOrUpdate(
+                new CommentEntity { Name = "Racer", Body = "Awesome game", Game = context.Games.Find("1") },
+                new CommentEntity { Name = "Chess_Player", Body = "Still popular", Game = context.Games.Find("3") },
+                new CommentEntity { Name = "Chess_Guru", Body = "Agree", ParentName = "Chess_Player", Game = context.Games.Find("3") },
+                new CommentEntity { Name = "The Cave", Body = "Perfect story", Game = context.Games.Find("4") }
+                );
+            context.SaveChanges();
+
+            //Filling Game with Genres binding
+            context.GameGenres.AddOrUpdate(
+                new GameGenreEntity { Game = context.Games.Find("1"), GameName = context.Games.Find("1").Name, GenreName = "Races"},
+                //TODO: game can be with different genres
+                //new GameGenreEntity { Game = context.Games.Find("1"), GameName = context.Games.Find("1").Name, GenreName = "arcade"},
+                new GameGenreEntity { Game = context.Games.Find("2"), GameName = context.Games.Find("2").Name, GenreName = "RPG"},
+                new GameGenreEntity { Game = context.Games.Find("3"), GameName = context.Games.Find("3").Name, GenreName = "Puzzle Skill"},
+                new GameGenreEntity { Game = context.Games.Find("4"), GameName = context.Games.Find("4").Name, GenreName = "Adventure"}
+                );
+            context.SaveChanges();
+
+            //Filling Game with Platform type binding
+            context.GamePlatformTypes.AddOrUpdate(
+                new GamePlatformTypeEntity { Game = context.Games.Find("1"), GameName = context.Games.Find("1").Name, TypeName = "Desktop" },
+                new GamePlatformTypeEntity { Game = context.Games.Find("2"), GameName = context.Games.Find("2").Name, TypeName = "Desktop" },
+                new GamePlatformTypeEntity { Game = context.Games.Find("3"), GameName = context.Games.Find("3").Name, TypeName = "Desktop" },
+                new GamePlatformTypeEntity { Game = context.Games.Find("4"), GameName = context.Games.Find("4").Name, TypeName = "Desktop" }
+                );
+            context.SaveChanges();
         }
     }
 }
